a1b51509adf169e4392fc15ecbb2e935a009a8da
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-15 18:28:14 +0530

    add authentication

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 const Header = () => {
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
@@ -8,7 +9,9 @@ const Header = () => {
         <Image src={'/logo.svg'} alt='logo' width={40} height={40}/>
         <h1 className="text-base font-bold md:text-2xl">Prepzi </h1>
       </div>
+      <Link href={'/dashboard'}>
       <Button>Get Started</Button>
+      </Link>
     </nav>
   )
 }
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const Hero = () => {
   return (
     <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
@@ -63,8 +64,9 @@ const Hero = () => {
             delay: 1,
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
-        >
+        > <Link href={'/dashboard'}>
           <Button size={'lg'}>Explore Now</Button>
+          </Link>
           <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
             Contact Support
           </button>
